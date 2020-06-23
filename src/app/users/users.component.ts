4fb8ff6a6be6da754d79c120195d74f56ff24d9c
clintonoliera <clintonoliera391@gmail.com>
2020-06-23 15:56:47 +0300

    added a styling to the user and repos components

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserserviceService} from '../userservice/userservice.service'
+import { Users } from '../users'
+import { Repository } from '../repository'
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  user: Users;
+  repo: Repository;
 
-  ngOnInit(): void {
+  constructor(public myService: UserserviceService, private repoService: UserserviceService) { }
+
+  searchs(searchName) {
+    this.myService.searchUSer(searchName).then(
+      (success)=>{
+        this.user = this.myService.foundUser;
+        console.log(success);
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
+    this.repoService.getReopos(searchName).then(
+      (results)=>{
+        this.repo =this.repoService.allRepos
+        console.log(this.repo);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+}
+
+  ngOnInit(){
+    this.searchs('ClintonOliera');
   }
 
 }
